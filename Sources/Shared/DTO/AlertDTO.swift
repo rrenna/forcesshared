f091dc0fce42487b21b88af462a02af5bb02478c
Ryan Renna <ryan.renna@mark43.com>
2022-04-07 21:30:36 -0400

    Updated `AlertDTO`

--- Sources/Shared/DTO/AlertDTO.swift
+++ Sources/Shared/DTO/AlertDTO.swift
@@ -1,5 +1,5 @@
 //
-//  Alert.swift
+//  AlertDTO.swift
 //  
 //
 //  Created by Ryan Renna on 2022-04-07.
@@ -8,7 +8,12 @@
 import Foundation
 
 public struct AlertDTO: Codable {
+
+    public let id: UUID?
+    public let name: String
     
-  public let id: UUID?
-  public let name: String
+    public init(id: UUID?, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
