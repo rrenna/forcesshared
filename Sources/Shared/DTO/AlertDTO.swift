c59e0f223a6e4779bbd9c755a8e0bbb31457eb61
Ryan Renna <ryan.renna@mark43.com>
2022-04-09 20:25:30 -0400

    Added value to AlertDTO.

--- Sources/Shared/DTO/AlertDTO.swift
+++ Sources/Shared/DTO/AlertDTO.swift
@@ -11,12 +11,14 @@ public struct AlertDTO: Codable {
 
     public let id: UUID?
     public let name: String
+    public let value: String?
     public let latestTitle: String?
     public let latestURL: String?
     
-    public init(id: UUID?, name: String, latestTitle: String?, latestURL: String?) {
+    public init(id: UUID?, name: String, value: String?, latestTitle: String?, latestURL: String?) {
         self.id = id
         self.name = name
+        self.value = value
         self.latestTitle = latestTitle
         self.latestURL = latestURL
     }
