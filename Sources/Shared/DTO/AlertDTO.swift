5b28a563198a9e3b9f805df582eeb95b9429afc8
Ryan Renna <ryan.renna@mark43.com>
2022-04-28 09:27:30 -0400

    Added groups to alertDTO.

--- Sources/Shared/DTO/AlertDTO.swift
+++ Sources/Shared/DTO/AlertDTO.swift
@@ -14,12 +14,14 @@ public struct AlertDTO: Codable {
     public let value: String?
     public let latestTitle: String?
     public let latestURL: String?
+    public let group: String?
     
-    public init(id: UUID?, name: String, value: String?, latestTitle: String?, latestURL: String?) {
+    public init(id: UUID?, name: String, value: String?, latestTitle: String?, latestURL: String?, group: String?) {
         self.id = id
         self.name = name
         self.value = value
         self.latestTitle = latestTitle
         self.latestURL = latestURL
+        self.group = group
     }
 }
