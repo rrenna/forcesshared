fd851b685eab10e79af8f3f61f6ea6235a123725
Ryan Renna <ryan.renna@mark43.com>
2022-05-20 05:04:35 -0400

    Added value to create alert

--- Sources/Shared/DTO/CreateAlertDTO.swift
+++ Sources/Shared/DTO/CreateAlertDTO.swift
@@ -10,13 +10,15 @@ import Foundation
 public struct CreateAlertDTO: Codable {
 
     public let name: String
+    public let value: String
     public let group: String?
     public let rssURL: String
     public let keywords: [String]
     
-    public init(name: String,  group: String?, rssURL: String, keywords: [String]) {
+    public init(name: String, value: String, group: String?, rssURL: String, keywords: [String]) {
         
         self.name = name
+        self.value = value
         self.rssURL = rssURL
         self.group = group
         self.keywords = keywords
