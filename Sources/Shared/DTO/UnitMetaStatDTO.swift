5b2e025c9d80bc396d6ca175c539a1c889db805c
Ryan Renna <ryan.renna@mark43.com>
2022-07-11 15:06:46 -0400

    Fixed typo.

--- Sources/Shared/DTO/UnitMetaStatDTO.swift
+++ Sources/Shared/DTO/UnitMetaStatDTO.swift
@@ -12,14 +12,14 @@ public struct UnitMetaStatDTO: Codable {
     public var name: String
     public var percentOfArmiesContaining: Float
     public var meanAmount: Float
-    public var mediumAmount: Float
+    public var medianAmount: Float
     public var mostCommonAmount: Float
     
-    public init(name: String, percentOfArmiesContaining: Float, meanAmount: Float, mediumAmount: Float, mostCommonAmount: Float) {
+    public init(name: String, percentOfArmiesContaining: Float, meanAmount: Float, medianAmount: Float, mostCommonAmount: Float) {
         self.name = name
         self.percentOfArmiesContaining = percentOfArmiesContaining
         self.meanAmount = meanAmount
-        self.mediumAmount = mediumAmount
+        self.medianAmount = medianAmount
         self.mostCommonAmount = mostCommonAmount
     }
 }
